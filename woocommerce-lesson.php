80107bc827cf8b42b2324f5930a96fcd47ab385a
SolomashenkoRoman <solomashenko.roman.1991@gmail.com>
2017-04-27 19:53:46 +0300

    Реализация класса интеграции WC

--- woocommerce-lesson.php
+++ woocommerce-lesson.php
@@ -37,6 +37,26 @@ if (! in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', 
 if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
 
 
+
+
+    add_action( 'plugins_loaded', 'wc_les_my_init', 0 );
+
+    function wc_les_my_init() {
+        require_once dirname(__FILE__) . '/My_WC_Integration.php';
+    }
+
+    add_filter( 'woocommerce_integrations', 'add_woocommerce_integrations' );
+    function add_woocommerce_integrations( $integrations ) {
+        $integrations[] = 'My_WC_Integration';
+        return $integrations;
+    }
+
+
+    add_action( 'woocommerce_api_callback', 'callback_handler' );
+    function callback_handler(){
+        error_log('Run callback');
+    }
+
     // Include our Gateway Class and Register Payment Gateway with WooCommerce
     add_action( 'plugins_loaded', 'wc_les_liqpay_init', 0 );
 
@@ -306,6 +326,50 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
     add_action( 'woocommerce_review_order_before_cart_contents', 'tutsplus_validate_order' , 10 );
     add_action( 'woocommerce_after_checkout_validation', 'tutsplus_validate_order' , 10 );
 
+
+
+add_filter( 'woocommerce_get_sections_products', 'wcslider_add_section' );
+function wcslider_add_section( $sections ) {
+    $sections['wcslider'] = __( 'WC Slider', 'text-domain' );
+    return $sections;
+}
+add_filter( 'woocommerce_get_settings_products', 'wcslider_all_settings', 20, 2 );
+function wcslider_all_settings( $settings, $current_section ) {
+    /**
+     * Check the current section is what we want
+     **/
+    if ( $current_section == 'wcslider' ) {
+        $settings_slider = array();
+        // Add Title to the Settings
+        $settings_slider[] = array( 'name' => __( 'WC Slider Settings', 'text-domain' ), 'type' => 'title', 'desc' => __( 'The following options are used to configure WC Slider', 'text-domain' ), 'id' => 'wcslider' );
+        // Add first checkbox option
+        $settings_slider[] = array(
+            'name'     => __( 'Auto-insert into single product page', 'text-domain' ),
+            'desc_tip' => __( 'This will automatically insert your slider into the single product page', 'text-domain' ),
+            'id'       => 'wcslider_auto_insert',
+            'type'     => 'checkbox',
+            'css'      => 'min-width:300px;',
+            'desc'     => __( 'Enable Auto-Insert', 'text-domain' ),
+        );
+        // Add second text field option
+        $settings_slider[] = array(
+            'name'     => __( 'Slider Title', 'text-domain' ),
+            'desc_tip' => __( 'This will add a title to your slider', 'text-domain' ),
+            'id'       => 'wcslider_title',
+            'type'     => 'text',
+            'desc'     => __( 'Any title you want can be added to your slider with this option!', 'text-domain' ),
+        );
+        
+        $settings_slider[] = array( 'type' => 'sectionend', 'id' => 'wcslider' );
+        return $settings_slider;
+    
+    /**
+     * If not, return the standard settings
+     **/
+    } else {
+        return $settings;
+    }
+}
 
 
 } else {
